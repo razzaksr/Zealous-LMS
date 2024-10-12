278f62c5dcf4635f188c7b97077ab1670b738835
Noorul Ashfaq A <noorulashfaq786@gmail.com>
2024-10-12 10:17:10 +0530

    Coding tests flow services & tests written

--- api/index.js
+++ api/index.js
@@ -15,7 +15,7 @@ const tagController = require('./controllers/tagController');
 const testcaseController = require('./controllers/testcaseController');
 const userController = require("./controllers/userController");
 const userTestResultController = require('./controllers/userTestResultController');
-const userDashboardApi = require('./services/userDashboard');
+const userDashboardApi = require('./controllers/userDashboard');
 
 const app = express();
 const PORT = process.env.PORT;
