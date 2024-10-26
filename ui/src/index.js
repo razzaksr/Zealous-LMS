12d96c527fe7a44f24227b116bb4594c661e3bee
DHARUN-PRAKASH <dharunprakash2904@gmail.com>
2024-10-26 20:46:57 +0530

    home page

--- ui/src/index.js
+++ ui/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Signup from './pages/signup';
 import Signin from './pages/signin';
+import Home from './pages/home';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -20,6 +21,7 @@ root.render(
         <Routes>
           <Route exact path='/' element={<Signin />} />
           <Route exact path='/signup' element={<Signup />} />
+          <Route exact path='/home' element={<Home />} />
         </Routes>
       </BrowserRouter>
     )
